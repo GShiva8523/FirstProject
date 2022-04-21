3c5662291ff6c65b14b9aca558adc67721fe6a81
sandy <shiva.sandy.129@gmail.com>
2022-04-21 15:11:22 +0530

    added

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ function calling(){
     let email=document.getElementById("in1").value;
     let password=document.getElementById("in2").value;
     if(email==="shiva.gade.129@gmail.com"&&password==="shiva123"){
-        window.open("Actors.html")
+        window.location.href("Actors.html")
     }
 
 
