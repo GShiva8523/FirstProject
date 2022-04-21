bf70505bdd595b111b0c5b2c9ac4f9cf4ff01611
sandy <shiva.sandy.129@gmail.com>
2022-04-21 15:21:28 +0530

    added

--- Index.js
+++ Index.js
@@ -2,8 +2,10 @@
 function calling(){
     let email=document.getElementById("in1").value;
     let password=document.getElementById("in2").value;
-    if(email==="shiva.gade.129@gmail.com"&&password==="shiva123"){
-        window.location.href("Actors.html")
+    if(email==="shiva"&&password==="sh"){
+       window.location="Actors.html"
+    }else{
+     alert("Invalid Username and Password")
     }
 
 
